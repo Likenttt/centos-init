4cf58b4e15976a7fc5303b5ce4463664bfc810fe
Chuanyi <chuanyi.work@gmail.com>
2023-04-12 16:17:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-sudo yum update -y && sudo yum install -y epel-release && sudo yum install -y nginx git python3 nodejs npm zsh
+sudo yum update -y && sudo yum install -y epel-release
+sudo yum install -y nginx git python3 nodejs npm zsh vim curl wget gcc-c++ make nodejs npm certbot util-linux-user
+sudo yum groupinstall -y "Development Tools"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
