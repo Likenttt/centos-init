19e227a425d25edf905b895b34aeeda27f3cccad
Chuanyi <chuanyi.work@gmail.com>
2023-04-24 21:42:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sudo yum update -y && sudo yum install -y epel-release
-sudo yum install -y nginx git python3 nodejs npm zsh vim curl wget gcc-c++ make nodejs npm certbot util-linux-user
+sudo yum install -y nginx git python3 nodejs npm zsh vim curl wget gcc-c++ make nodejs npm certbot util-linux-user httpd-tools 
 sudo yum groupinstall -y "Development Tools"
-
+sudo dnf install -y python3-certbot-nginx
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --reload
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s /usr/bin/zsh
